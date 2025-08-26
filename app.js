da9f4e9530e96590bf4d93f4608801fdb8a9f51e
Vinson <vinsonhe2@gmail.com>
2025-08-26 11:37:58 -0700

    chore: set up EJS templating engine

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 import express from "express";
+import path from "node:path";
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello from index!"));
+const assetsPath = path.join(import.meta.dirname, "public");
+app.use(express.static(assetsPath));
+
+app.set("views", path.join(import.meta.dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => res.render("index", { variable: "Hello from index!" }));
 app.get("/new", (req, res) => res.send("Hello from new message form!"));
 
 const PORT = 3000;
