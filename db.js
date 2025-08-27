d6d28f98ec01ff30e9ba76149c81266d68678069
Vinson <vinsonhe2@gmail.com>
2025-08-26 17:32:03 -0700

    feat(model): set up mock database

---
+++ db.js
@@ -0,0 +1,14 @@
+const messages = [
+  {
+    text: "Hello, world!",
+    user: "First computer",
+    added: new Date(),
+  },
+  {
+    text: "There's a moth in my computer!",
+    user: "Harvard",
+    added: new Date(),
+  },
+];
+
+export { messages };
