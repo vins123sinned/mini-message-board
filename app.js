9dea7ab98f679a32fb86fac43f25d50948eb7ca0
Vinson <vinsonhe2@gmail.com>
2025-08-26 17:45:54 -0700

    feat(error): create and handle custom errors
    - Add error handler middleware
    - Create CustomNotFoundError class

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ app.set("view engine", "ejs");
 // router configuration
 app.use("/", indexRouter);
 
+// error middlewares
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
 const PORT = 3000;
 app.listen(PORT, (error) => {
   if (error) throw error;
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,9 @@
 import { messages } from "../db.js";
+import { CustomNotFoundError } from "../errors/customNotFoundError.js";
 
 async function getIndexPage(req, res) {
+  if (!messages) throw new CustomNotFoundError("Can't get messages");
+
   res.render("index", { messages: messages });
 }
 
