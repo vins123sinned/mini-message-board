43bdaff866f155ffed2642ab6eccf04b301a13ad
Vinson <vinsonhe2@gmail.com>
2025-08-28 06:04:38 -0700

    feat(messages): add UUID and message view link
    Each message is now assigned a random UUID in db.js.
    index.ejs uses these UUIDs to generate links to a specific message's
    page.

--- db.js
+++ db.js
@@ -3,11 +3,13 @@ const messages = [
     text: "Hello, world!",
     user: "First computer",
     added: new Date(),
+    id: crypto.randomUUID(),
   },
   {
     text: "There's a moth in my computer!",
     user: "Harvard",
     added: new Date(),
+    id: crypto.randomUUID(),
   },
 ];
 
